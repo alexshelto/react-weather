fe79612ef46227696b14e26bc3731efe02018ced
alexshelto <alexshelto@icloud.com>
2020-09-03 11:57:49 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const api = {
+  key: 'f42e61576713df989fd80dd11d4594b8',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+
+
+
 function App() {
-  return (
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+     }
+  }
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <main>
+          <div className="search-box">
+            <input type="text"
+             className="search-bar"
+             placeholder="Search.."
+             value={query}
+             onKeyPress={search}
+             onChange={e => setQuery(e.target.value)} />
+          </div>
+          
+          <div className="location-box">
+            <div className="location">
+              <p>Athens, Ohio</p>
+            </div>
+            
+            <div className="time">
+              <p>4:20pm </p>
+            </div>
+          </div>
+
+            <div className="weather-box">
+              <div className="temp">
+                <h2> 0 degrees </h2>
+              </div>
+            
+            <div className="conditions">
+              <p> Sunny </p>
+            </div>
+          </div>
+        </main>
     </div>
   );
+
 }
+
 
 export default App;
